96070806457e9331274634d023bf2cd1b5e32405
Joel Velis <joelvelis94@gmail.com>
2019-01-12 16:51:13 -0500

    checkpoint fixes

---
+++ src/components/ToDo.js
@@ -0,0 +1,15 @@
+ import React, { Component } from 'react';
+
+ class ToDo extends Component {
+   render() {
+     return (
+     	<div>
+       <li>Finish the Component section of React</li>
+       <li>A second todo will go here</li>
+       	</div>
+  
+     );
+   }
+ }
+ 
+ export default ToDo;
