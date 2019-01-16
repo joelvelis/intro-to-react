43e5c6c303db739c645421ad7e56a4fc19625448
Joel Velis <joelvelis94@gmail.com>
2019-01-16 18:54:14 -0500

    assignment complete - events

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@
 
  class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
-        { description: 'Walk the cat', isCompleted: true },
-        { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCompleted: false }
-        ],
+    super(props); //super tells the component to first cll its parent constructor function and then it own
+      this.state = {  //this.state is an object 
+        todos: [      //todos is a property of the this.state object and is also an array
+        { description: 'Walk the cat', isCompleted: true },             //description & isCompleted are properties of an object
+        { description: 'Throw the dishes away', isCompleted: false },  
+        { description: 'Buy new dishes', isCompleted: false }          
+      ],
         newTodoDescription: ''
-      };    
     };
-  
+  }
+
+ deleteTodo(index) {
+    this.setState({ todos: this.state.todos.filter((_, todoItem) => todoItem !== index)}
+    );
+}
 
   handleChange(e) {
      this.setState({ newTodoDescription: e.target.value })
@@ -23,32 +27,38 @@
   handleSubmit(e) {
      e.preventDefault();
      if (!this.state.newTodoDescription) { return }
-     const newTodo = { description: this.state.newTodoDescription, isCompleted: false }; 
+     const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
      this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
    }
 
-    toggleComplete(index) {
+   toggleComplete(index) {
     const todos = this.state.todos.slice();
     const todo = todos[index];
     todo.isCompleted = todo.isCompleted ? false : true;
     this.setState({ todos: todos });
   }
 
+
    render() {
      return (
-       <div className="App">
-        <ul>
-           { this.state.todos.map( (todo, index) => 
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
-          )}
-        </ul>
-        <form onSubmit={ (e) => this.handleSubmit(e) }>
+       <div className="App"> 
+          <ul>
+           { this.state.todos.map( (todo, index) => /* here we are using the map method on todos. It is enclosed in curly brackets to tell JSX to treat it as JavaScript code */
+             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete= { () => this.toggleComplete(index) }  deleteTodo ={ () => this.deleteTodo(index) } /> /* here we are calling the ToDo component and adding attributes to it */
+           )}
+         </ul>
+         <form onSubmit={ (e) => this.handleSubmit(e) }>
            <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
            <input type="submit" />
-         </form>       
-       </div>
+         </form>
+       </div> /* this set of code (line 21-23) returns a <ToDo> component for each this.state.todos */
      );
    }
  }
+   
+ 
+ 
 
  export default App;
+
+
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,9 +4,11 @@
    render() {
      return (
        	<li>
-         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <input type="checkbox" checked={this.props.isCompleted} onChange= { this.props.toggleComplete } />
          <span>{ this.props.description }</span>
+         <input type="button" value="Delete" onClick={ this.props.deleteTodo } />
        </li>
+  
   
      );
    }
